03e99bec915a8c5f7d0029e2c268f46b53579f25
viro <twikista@gmail.com>
2021-09-13 02:57:09 +0100

    saved key elelments to variables in JavaScript

---
+++ js/app.js
@@ -0,0 +1,8 @@
+//save the grid parent to a variable
+const parent = document.querySelector(".parent");
+//save the range slider to a variable
+const slider = document.querySelector(".range-slider");
+//variable that dsplays the value of the range slider
+let sliderValue = document.querySelector(".slider-value");
+//blah
+const sliderBtn = document.querySelector(".slider-wrapper");
