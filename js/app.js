57d59fa5ccf2791d2fb7a0285c47cbbe0a357392
viro <twikista@gmail.com>
2021-09-13 03:29:44 +0100

    added the default grid functionality using the defaultGrid function

--- js/app.js
+++ js/app.js
@@ -4,5 +4,28 @@ const parent = document.querySelector(".parent");
 const slider = document.querySelector(".range-slider");
 //variable that dsplays the value of the range slider
 let sliderValue = document.querySelector(".slider-value");
-//blah
+//grab the btn parent element
 const sliderBtn = document.querySelector(".slider-wrapper");
+//set the value of default grid
+const defaultGrid = 16;
+// define custom grid
+let customGrid = 0;
+
+//function that implemements the functionalities of the sketch pad
+function sketchPad() {
+  createDefaultGrid();
+}
+
+//function that creates default number of divs in the sketch pad
+function createDefaultGrid() {
+  for (let i = 1; i <= defaultGrid ** 2; i++) {
+    let div = document.createElement("div");
+    div.className = `grid`;
+    parent.append(div);
+    sliderValue.textContent = `${defaultGrid} x ${defaultGrid}`;
+  }
+  gridLayout(defaultGrid, defaultGrid);
+  customGrid = defaultGrid;
+}
+
+sketchPad();
